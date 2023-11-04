8f8c514900ac01e69eb2d84cbc2735585659c13c
Henalecam <henriquealexandredec@gmail.com>
2023-11-04 19:33:06 -0300

    Received URL Current Site

--- background.js
+++ background.js
@@ -1,6 +1,24 @@
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("The color is green.");
-    });
-  }
+  chrome.storage.sync.set({color: '#3aa757'}, function() {
+    console.log('The color is green.');
+  });
+});
+
+async function getCurrentTab() {
+  let queryOptions = { active: true, lastFocusedWindow: true };
+  // `tab` will either be a `tabs.Tab` instance or `undefined`.
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+ //console.log("tabId: " + tabId + " changeInfo: " + changeInfo + " tab: " + tab);
+ //console.log(changeInfo);
+ //console.log(tab);
+ //console.log(tab.url);
+ getCurrentTab().then((tab) => {
+  console.log(tab.url);
+  });
+}
 );
+
---
+++ popup.js
@@ -0,0 +1,7 @@
+async function getCurrentTab() {
+  console.log('teste');
+  let queryOptions = { active: true, lastFocusedWindow: true };
+  // `tab` will either be a `tabs.Tab` instance or `undefined`.
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
