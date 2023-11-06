63a7e815f21596d91be7ed6ab8daf78ee901d001
Henrique Alexandre <henriquealexandredec@gmail.com>
2023-11-05 22:24:15 -0300

    Changing operating system

--- popup.js
+++ popup.js
@@ -1,7 +1,15 @@
-function getCurrentTabUrl() {
-  console.log('teste')
-}
+const addCart = document.querySelector("#addCart");
+const clear = document.querySelector("#clear");
+const checkout = document.querySelector("#checkout");
+const close = document.querySelector("#close");
+const editButtons = document.querySelectorAll(".editButton");
+const deleteButtons = document.querySelectorAll(".deleteButton");
 
-document.addEventListener("console", function () {
-  console.log('teste')
-})
+
+document.addEventListener('DOMContentLoaded', function() {
+  var link = document.getElementById('addCart');
+  console.log(link, addCart);
+  link.addEventListener('click', function(){
+    document.body.style.backgroundColor = 'blue';
+  })
+});
